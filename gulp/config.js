64e6ded81ef2858f59154c448b07dd97a9c3e730
Tuấn Lưu Văn <tuanlv5490@gmail.com>
2017-06-17 17:27:55 +0700

    Initial structure

---
+++ gulp/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  JS: [{
+    SOURCES: [
+      './source/js/main.js'
+    ],
+    OUT_FILE: 'main.min.js'
+  }],
+  JS_OUT_DIR: './dist/js/',
+  SASS_SOURCE: './source/sass/**/*.scss',
+  SASS_OUT_DIR: './dist/css/'
+};
